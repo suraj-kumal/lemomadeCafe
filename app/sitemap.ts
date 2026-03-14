4656cb188229f29702599dee974f5150955ae9cd
Suraj Kumal <zerocoolofficial45@gmail.com>
2026-03-14 19:42:38 +0545

    sitemap updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://lemonadecafe.com.np",
+      url: "https://lemonadecafe.vercel.app",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://lemonadecafe.com.np/menu",
+      url: "https://lemonadecafe.vercel.app/menu",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
